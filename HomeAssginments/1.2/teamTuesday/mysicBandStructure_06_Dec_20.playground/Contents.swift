277da118b9f567d91214385c4554aeba846634fc
Mary Sneg <lazorenkoua@gmail.com>
2020-12-06 21:25:48 +0100

    part of task

--- HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
-// some coment
-// another coment
+struct MusicBand <BandType> {
+    var band: BandType
+    var country: String
+
+    func bandByCountry() -> String {
+        return "\"\(band)\" is from \(country)"
+    }
+   
+}
+
+let bandsList = [
+    MusicBand(band: "The Beatles", country: "United Kingdom"),
+    MusicBand(band: "Guns n Roses", country: "United States"),
+    MusicBand(band: "Scorpions", country: "Germany"),
+    MusicBand(band: "ABBA", country: "Sweden"),
+    MusicBand(band: "Daft punk", country: "France"),
+    MusicBand(band: "Nightwish", country: "Finland"),
+    MusicBand(band: "AC/DC", country: "Australia")]
+
+
