feaa84b317118735d87ba4f933cea54958582f03
Mary Sneg <lazorenkoua@gmail.com>
2020-12-07 20:00:52 +0100

    fix var->l et change "]" position

--- HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
@@ -1,21 +1,25 @@
 import UIKit
 struct MusicBand <BandType> {
-    var band: BandType
-    var country: String
+    let name: BandType
+    let country: String
 
     func bandByCountry() -> String {
-        return "\"\(band)\" is from \(country)"
+        return "\"\(name)\" is from \(country)"
     }
    
 }
 
 let bandsList = [
-    MusicBand(band: "The Beatles", country: "United Kingdom"),
-    MusicBand(band: "Guns n Roses", country: "United States"),
-    MusicBand(band: "Scorpions", country: "Germany"),
-    MusicBand(band: "ABBA", country: "Sweden"),
-    MusicBand(band: "Daft punk", country: "France"),
-    MusicBand(band: "Nightwish", country: "Finland"),
-    MusicBand(band: "AC/DC", country: "Australia")]
+    MusicBand(name: "The Beatles", country: "United Kingdom"),
+    MusicBand(name: "Guns n Roses", country: "United States"),
+    MusicBand(name: "Scorpions", country: "Germany"),
+    MusicBand(name: "ABBA", country: "Sweden"),
+    MusicBand(name: "Daft punk", country: "France"),
+    MusicBand(name: "Nightwish", country: "Finland"),
+    MusicBand(name: "AC/DC", country: "Australia")
+            ]
+
+
+
 
 
