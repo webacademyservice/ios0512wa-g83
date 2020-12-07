bd35feecc51580ccf479a495c84d3fbf86fdc20c
Oleksandr Kulahin <a.yu.kulagin@gmail.com>
2020-12-07 17:20:25 +0200

    Musician structure

--- HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
@@ -1,3 +1,20 @@
 import UIKit
 // some coment
 // another coment
+
+//Musician structure
+
+struct Musician
+{
+    var name: String
+    var familyName: String
+    var age: Int
+    var titleInBand: String
+    
+    func printInfoAboutMusician(){
+        print("Name: \(name) \(familyName)\nAge: \(age)\nTitle: \(titleInBand)")
+    }
+}
+
+var musician: Musician = Musician(name: "Alex", familyName: "Kualhin", age: 31, titleInBand: "Bass")
+musician.printInfoAboutMusician()
