ca6baed59d77799ba2d687aefb4f11eb9d1f36d0
Oleksandr Kulahin <a.yu.kulagin@gmail.com>
2020-12-10 01:57:17 +0200

    Add date

--- HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
@@ -1,28 +1,50 @@
 import UIKit
+import Foundation
 // some coment
 // another coment
 
 //Musician structure
 
-struct Musician <BandName>
+struct Musician<BandName>
 {
     var bandName: BandName
     let fullName: String
     let titleInBand: String
-    let periodInBand: String
+    let periodInBand: DateInterval
+    //var startYear: Int
+    //var endYear: Int
+
+    init(bandName: BandName? = nil,
+          fullName: String? = nil,
+          titleInBand: String? = nil,
+          startYear: Int? = nil,
+          endYear: Int? = nil)
+    {
+        let calendar = Calendar.current
+        self.bandName = (bandName)!
+        self.fullName = (fullName)!
+        self.titleInBand = (titleInBand)!
+        //self.startYear = (startYear)!
+        //self.endYear = (endYear)!
+        self.periodInBand = DateInterval(start: calendar.date(from: DateComponents(year: startYear))!,
+                                         end: calendar.date(from: DateComponents(year:endYear))!)
+    }
     
-    func printInfoAboutMusician(){
-        print("\(bandName): \(fullName) - \(titleInBand) (\(periodInBand))")
+    func printInfoAboutMusician()
+    {
+    
+        print("\(bandName): \(fullName) - \(titleInBand)")
     }
 }
 
-var musicianList = [
-    Musician(bandName: "Beatels", fullName: "John Lennon", titleInBand: "vocals, guitars, keyboards, harmonica, bass", periodInBand: "1960-1970"),
-    Musician(bandName: "Beatels", fullName: "Paul McCartney", titleInBand: "vocals, bass, guitars, keyboards, drums", periodInBand: "1960-1970"),
-    Musician(bandName: "Beatels", fullName: "George Harrison", titleInBand: "guitars, vocals, sitar, keyboards, bass", periodInBand: "1960-1970"),
-    Musician(bandName: "Beatels", fullName: "Ringo Starr", titleInBand: "drums, percussion, vocals", periodInBand: "1962-1970")
+var musiciansList = [
+    Musician(bandName: "Beatels", fullName: "John Lennon", titleInBand: "vocals, guitars, keyboards, harmonica, bass", startYear: 1960, endYear: 1970),
+    Musician(bandName: "Beatels", fullName: "Paul McCartney", titleInBand: "vocals, bass, guitars, keyboards, drums", startYear: 1960, endYear: 19700),
+    Musician(bandName: "Beatels", fullName: "George Harrison", titleInBand: "guitars, vocals, sitar, keyboards, bass", startYear: 1960, endYear: 1970),
+    Musician(bandName: "Beatels", fullName: "Ringo Starr", titleInBand: "drums, percussion, vocals",startYear: 1962, endYear: 1970)
 ]
 
-for element in musicianList{
+for element in musiciansList
+{
     element.printInfoAboutMusician()
 }
