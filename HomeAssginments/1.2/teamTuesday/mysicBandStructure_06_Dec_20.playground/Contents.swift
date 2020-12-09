15bd73a5f57b83bf8e6e9c37f2701dee47e5fdc5
Oleksandr Kulahin <a.yu.kulagin@gmail.com>
2020-12-09 14:29:19 +0200

    Add generic

--- HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
@@ -4,22 +4,23 @@ import UIKit
 
 //Musician structure
 
-struct Musician
+struct Musician <BandName>
 {
+    var bandName: BandName
     let fullName: String
     let titleInBand: String
     let periodInBand: String
     
     func printInfoAboutMusician(){
-        print("\(fullName) - \(titleInBand) (\(periodInBand))")
+        print("\(bandName): \(fullName) - \(titleInBand) (\(periodInBand))")
     }
 }
 
 var musicianList = [
-Musician(fullName: "John Lennon", titleInBand: "vocals, guitars, keyboards, harmonica, bass", periodInBand: "1960-1970"),
-    Musician(fullName: "Paul McCartney", titleInBand: "vocals, bass, guitars, keyboards, drums", periodInBand: "1960-1970"),
-    Musician(fullName: "George Harrison", titleInBand: "guitars, vocals, sitar, keyboards, bass", periodInBand: "1960-1970"),
-    Musician(fullName: "Ringo Starr", titleInBand: "drums, percussion, vocals", periodInBand: "1962-1970")
+    Musician(bandName: "Beatels", fullName: "John Lennon", titleInBand: "vocals, guitars, keyboards, harmonica, bass", periodInBand: "1960-1970"),
+    Musician(bandName: "Beatels", fullName: "Paul McCartney", titleInBand: "vocals, bass, guitars, keyboards, drums", periodInBand: "1960-1970"),
+    Musician(bandName: "Beatels", fullName: "George Harrison", titleInBand: "guitars, vocals, sitar, keyboards, bass", periodInBand: "1960-1970"),
+    Musician(bandName: "Beatels", fullName: "Ringo Starr", titleInBand: "drums, percussion, vocals", periodInBand: "1962-1970")
 ]
 
 for element in musicianList{
