7c1c1dc75e2bfc2b8d5ed45e76756a95e606c4f6
Alex Zaets <alexzaets1997@gmail.com>
2020-12-10 18:37:17 +0200

    struct Theatre

--- HomeAssginments/1.2/teamSunday/test.playground/Contents.swift
+++ HomeAssginments/1.2/teamSunday/test.playground/Contents.swift
@@ -1,3 +1,30 @@
-import UIKit
-
-var str = "Hello, playground"
+// Структура Театров
+struct Theatre <TheatresType> {
+    var theatresType:TheatresType
+    let city: String
+    let stockTickets: Int
+  }
+  //Театры
+  let firstTheatre = Theatre(theatresType: "Lvivskii Theatre", city: "Lviv", stockTickets: 349)
+  let secondTheatre = Theatre(theatresType: "Thetre on Podil", city: "Kiev", stockTickets: 400)
+  struct Afisha<AfishaType>{
+    let theatres: AfishaType
+    let actorName: String
+    let titleInSpectacle: String
+    let dateInSpectacle: String
+    func showAfisha() {
+      print("Сейчас Aктуально: \nТеатр: \(theatres) \nАктер: \(actorName) \nШоу: \(titleInSpectacle) \nДата: \(dateInSpectacle)")
+    }
+  }
+let firstAfisha = Afisha(
+      theatres: ("\(firstTheatre.theatresType), \(firstTheatre.city) \nКоличество Билетов: \(firstTheatre.stockTickets)"),
+      actorName: "Nurlan Saburov",
+      titleInSpectacle: "StendUp Show",
+      dateInSpectacle: "20.12.2020")
+    firstAfisha.showAfisha()
+    let secondAfisha = Afisha (
+      theatres: ("\(secondTheatre.theatresType), \(secondTheatre.city) \nКоличество Билетов: \(secondTheatre.stockTickets)"),
+      actorName: "Philip Kirkorov",
+      titleInSpectacle: "Best of the Best",
+      dateInSpectacle: "30.12.2020")
+    secondAfisha.showAfisha()
