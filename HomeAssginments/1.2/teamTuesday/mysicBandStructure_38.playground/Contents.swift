1da35f62f06aabb899091a270f861ace23efdacd
Mary Sneg <lazorenkoua@gmail.com>
2020-12-10 12:13:43 +0100

    some view change

--- HomeAssginments/1.2/teamTuesday/mysicBandStructure_38.playground/Contents.swift
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_38.playground/Contents.swift
@@ -9,5 +9,6 @@ struct Band <MusiciansType, AlbumsType> {
 }}
 let bandsList = [Band(name: "The Beatles", country: "United Kingdom", musiciansInBand: ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"], albumsOfBand: ["Something New", "Beatles 65", "Beatles VI"]),
                  Band(name: "Guns and Roses", country: "United States", musiciansInBand:[], albumsOfBand: []),
-    Band(name: "Scorpions", country: "Germany", musiciansInBand: [], albumsOfBand: []),
-    Band(name: "ABBA", country: "Sweden", musiciansInBand: [], albumsOfBand: [])]
+                 Band(name: "Scorpions", country: "Germany", musiciansInBand: [], albumsOfBand: []),
+                 Band(name: "ABBA", country: "Sweden", musiciansInBand: [], albumsOfBand: [])
+]
