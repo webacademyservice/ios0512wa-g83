23f57fd91d114112d2a06f7c16d5ddf27c24b4ff
Oleksandr Kulahin <a.yu.kulagin@gmail.com>
2020-12-10 17:21:04 +0200

    Change in Musician structure "periodInBand" to Date and add Dictionary field

--- HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_06_Dec_20.playground/Contents.swift
@@ -7,41 +7,47 @@ import Foundation
 
 struct Musician<BandName>
 {
+    var musicianHistory: Dictionary<DateInterval, BandName>
     var bandName: BandName
     let fullName: String
     let titleInBand: String
     let periodInBand: DateInterval
-    //var startYear: Int
-    //var endYear: Int
 
     init(bandName: BandName? = nil,
           fullName: String? = nil,
           titleInBand: String? = nil,
-          startYear: Int? = nil,
-          endYear: Int? = nil)
+          periodInBand: DateInterval? = nil)
     {
-        let calendar = Calendar.current
         self.bandName = (bandName)!
         self.fullName = (fullName)!
         self.titleInBand = (titleInBand)!
-        //self.startYear = (startYear)!
-        //self.endYear = (endYear)!
-        self.periodInBand = DateInterval(start: calendar.date(from: DateComponents(year: startYear))!,
-                                         end: calendar.date(from: DateComponents(year:endYear))!)
+        self.periodInBand = (periodInBand)!
+        self.musicianHistory = Dictionary<DateInterval, BandName>()
+        self.musicianHistory.updateValue((bandName)!, forKey: (periodInBand)!)
     }
     
     func printInfoAboutMusician()
     {
-    
-        print("\(bandName): \(fullName) - \(titleInBand)")
+        let dateFormatterGet = DateFormatter()
+        dateFormatterGet.dateFormat = "yyyy"
+
+        let dateFormatterPrint = DateFormatter()
+        dateFormatterPrint.dateFormat = "yyyy"
+
+        print("\(bandName): \(fullName) - \(titleInBand) (\(dateFormatterPrint.string(from: periodInBand.start))-\(dateFormatterPrint.string(from: periodInBand.end)))")
     }
 }
 
+func GenerateDateInterval(startYear: Int, endYear: Int) -> DateInterval
+{
+ return DateInterval.init(start: Calendar.current.date(from: DateComponents(year:startYear))!, end: Calendar.current.date(from: DateComponents(year:endYear))!)
+}
+
 var musiciansList = [
-    Musician(bandName: "Beatels", fullName: "John Lennon", titleInBand: "vocals, guitars, keyboards, harmonica, bass", startYear: 1960, endYear: 1970),
-    Musician(bandName: "Beatels", fullName: "Paul McCartney", titleInBand: "vocals, bass, guitars, keyboards, drums", startYear: 1960, endYear: 19700),
-    Musician(bandName: "Beatels", fullName: "George Harrison", titleInBand: "guitars, vocals, sitar, keyboards, bass", startYear: 1960, endYear: 1970),
-    Musician(bandName: "Beatels", fullName: "Ringo Starr", titleInBand: "drums, percussion, vocals",startYear: 1962, endYear: 1970)
+    Musician(bandName: "Beatels", fullName: "John Lennon", titleInBand: "vocals, guitars, keyboards, harmonica, bass", periodInBand: GenerateDateInterval(startYear: 1960, endYear: 1970)),
+    Musician(bandName: "Beatels", fullName: "Paul McCartney", titleInBand: "vocals, bass, guitars, keyboards, drums", periodInBand: GenerateDateInterval(startYear: 1960, endYear: 1970)),
+    Musician(bandName: "Beatels", fullName: "George Harrison", titleInBand: "guitars, vocals, sitar, keyboards, bass", periodInBand: GenerateDateInterval(startYear: 1960, endYear: 1970)),
+    Musician(bandName: "Beatels", fullName: "Ringo Starr", titleInBand: "drums, percussion, vocals", periodInBand: GenerateDateInterval(startYear: 1962, endYear: 1970))
 ]
 
 for element in musiciansList
