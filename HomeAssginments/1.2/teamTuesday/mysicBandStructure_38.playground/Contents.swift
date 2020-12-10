2d0ec7d8c11eec2dd7ad11730eaa37c4b8b02048
Mary Sneg <lazorenkoua@gmail.com>
2020-12-10 12:27:14 +0100

    function name camelCase

--- HomeAssginments/1.2/teamTuesday/mysicBandStructure_38.playground/Contents.swift
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_38.playground/Contents.swift
@@ -4,7 +4,7 @@ struct Band <MusiciansType, AlbumsType> {
     let country: String
     let musiciansInBand: Array <MusiciansType>
     var albumsOfBand: Array <AlbumsType>
-    func BandByCountry() -> String {
+    func bandByCountry() -> String {
         return "\(name)\" is from \(country)"
 }}
 let bandsList = [Band(name: "The Beatles", country: "United Kingdom", musiciansInBand: ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"], albumsOfBand: ["Something New", "Beatles 65", "Beatles VI"]),
