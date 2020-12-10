ca2529d5458208ca1a11781068819aff7f654bfd
AntonMoroz <moroz.antony@gmail.com>
2020-12-10 14:28:31 +0200

    Make struct for ship

---
+++ TeamThursday.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+struct Ship {
+   let name: String
+   let direction: String
+   let teamCapacity: Int
+   
+   func description() -> String {
+       return "Ship \"\(name)\" is going to \(direction) required team size \(teamCapacity)"
+
+   }
+}
+
+let ShipInfo = Ship(name:"Titanic", direction:"New-York", teamCapacity: 250)
+   
+print(ShipInfo.description())
+
+   var List =  [
+   Ship(name:"Titanic", direction:"New-York", teamCapacity: 250),
+   Ship(name: "Favolosa" , direction: "Oslo", teamCapacity: 200),
+   Ship(name: "Splendida", direction: "Barselona", teamCapacity: 150),
+   Ship(name: "Smeralda", direction: "Palermo", teamCapacity: 140),
+] 
