34f1490bb4b402c349e258a2f4525ebecdaf7afa
Mary Sneg <lazorenkoua@gmail.com>
2020-12-10 11:42:29 +0100

    Fixes + generics

---
+++ HomeAssginments/1.2/teamTuesday/mysicBandStructure_38.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+struct Band <MusiciansType, AlbumsType> {
+    let name: String
+    let country: String
+    let musiciansInBand: Array <MusiciansType>
+    var albumsOfBand: Array <AlbumsType>
+    func BandByCountry() -> String {
+        return "\(name)\" is from \(country)"
+}}
+let bandsList = [Band(name: "The Beatles", country: "United Kingdom", musiciansInBand: ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"], albumsOfBand: ["Something New", "Beatles 65", "Beatles VI"]),
+                 Band(name: "Guns and Roses", country: "United States", musiciansInBand:[], albumsOfBand: []),
+    Band(name: "Scorpions", country: "Germany", musiciansInBand: [], albumsOfBand: []),
+    Band(name: "ABBA", country: "Sweden", musiciansInBand: [], albumsOfBand: [])]
