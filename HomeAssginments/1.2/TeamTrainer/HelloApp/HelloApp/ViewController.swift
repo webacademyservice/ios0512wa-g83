25f9137b5598196ea9224707a064cccc8b9b19ba
Mykhailo Vorontsov <mykhailo.vorontsov@ly.st>
2020-12-12 09:58:44 +0000

    Add hello world project

---
+++ HomeAssginments/1.2/TeamTrainer/HelloApp/HelloApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloApp
+//
+//  Created by Mykhailo Vorontsov on 12/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
--- HomeAssginments/1.2/TeamTrainer/Pets.playground/Contents.swift
+++ HomeAssginments/1.2/TeamTrainer/Pets.playground/Contents.swift
@@ -1,12 +1,32 @@
-struct Pet: Equatable {
+
+protocol PetProtocol: Equatable {
+    var name: String { get }
+    func description() -> String
+}
+
+struct Pet: PetProtocol {
+    func description() -> String {
+        return "name is \(name)"
+    }
+
     let name: String
 }
 
-struct Owner<PetType: Equatable> {
+struct Owner<PetType: PetProtocol> {
     let pets: Array<PetType>
 
     func isOwn(pet: PetType) -> Bool  {
         return pets.contains(pet)
+    }
+}
+
+extension String: PetProtocol {
+    var name: String {
+        return self
+    }
+
+    func description() -> String {
+        return "string: \(self)"
     }
 }
 
@@ -23,10 +43,20 @@ let owner1 = Owner(pets: [
     Pet(name: "Bucks Bunny"),
 ])
 
-owner1.isOwn(pet: Pet(name: "Roger the Rabbit") )
+extension Array where Element: PetProtocol {
 
-// Incorrect:
-//owner0.isOwn(pet: Pet(name: "Roger the Rabbit") )
+    func bunnies() -> Self {
+        return filter({ element -> Bool in
+            return element.name.starts(with: "Bucks")
+        })
+    }
 
-// Incorrect:
-//owner1.isOwn(pet: "Roger the Rabbit")
+}
+
+let array = [
+    "Bucks Bunny"
+//    Pet(name: "Roger the Rabbit"),
+//    Pet(name: "Bucks Bunny"),
+]
+
+array.bunnies()
