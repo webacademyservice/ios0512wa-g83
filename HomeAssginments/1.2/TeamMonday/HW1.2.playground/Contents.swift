f1badba284f00d067291b1854ea4b5e5925c1a45
Elena Temerova <temerushka@iclod.com>
2020-12-13 18:45:32 +0200

    Add Protocol and extension to Course

--- HomeAssginments/1.2/TeamMonday/HW1.2.playground/Contents.swift
+++ HomeAssginments/1.2/TeamMonday/HW1.2.playground/Contents.swift
@@ -1,12 +1,20 @@
 import UIKit
 //// массив студентов 2 группы в строку
 ////тип студента Generic
-struct Course<StudentType:Equatable> {
-    
-    let nameOfCourse: String
-    let teacher: String
+
+protocol SomeProtocol {
+    var nameOfCourse: String {get set}
+    var teacher: String {get set}
+}
+
+struct Course<StudentType:Equatable>: SomeProtocol {
+    var nameOfCourse: String
+    var teacher: String
     let students: Array<StudentType>
-    
+
+}
+// ввела функцию как расширение структуры Курса
+extension Course {
     func findCommonStudents(studentsGroup:Array<StudentType>) -> Array<StudentType> {
         var  compareStud = Array<StudentType>()
         for i in 0...studentsGroup.count - 1 {
@@ -25,5 +33,7 @@ let firstGroup = ["Elena Temerova","Vitya Mandryk","Ilya Shubovich","Vasya Pupki
 let secondGroup = ["Karina Petlyura","Katya Shapoval","Vlad Yama","Vasya Pupkin","Brad Pitt"]
 
 let lesson = Course<String>(nameOfCourse: "Science", teacher: "O. V. Petrova", students: firstGroup)
-
 lesson.findCommonStudents(studentsGroup: secondGroup)
+
+
+
