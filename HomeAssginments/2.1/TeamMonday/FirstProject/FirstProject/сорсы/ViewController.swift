e8497897055ef7c2f57b0721859f2eafb162b31a
vitya <vitya1808200162@gmail.com>
2020-12-15 20:37:02 +0200

    Add First Project

---
+++ HomeAssginments/2.1/TeamMonday/FirstProject/FirstProject/сорсы/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstProject
+//
+//  Created by Vitya Mandryk on 12.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
