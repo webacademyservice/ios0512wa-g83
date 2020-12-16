a98da14ca321de8e3e2ee73d513a17c743a376a7
AntonMoroz <moroz.antony@gmail.com>
2020-12-16 20:57:55 +0200

    AddProject

---
+++ HomeAssginments/2.1/TeamThursday/ThursdayProject/ThursdayProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThursdayProject
+//
+//  Created by admin on 12/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
