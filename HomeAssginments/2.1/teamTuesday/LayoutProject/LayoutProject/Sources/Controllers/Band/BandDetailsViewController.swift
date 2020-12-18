48897f908810c3869d5e1bc7141d50faf68cf2b8
Mary Sneg <lazorenkoua@gmail.com>
2020-12-18 16:37:35 +0100

    add band code

--- HomeAssginments/2.1/teamTuesday/LayoutProject/LayoutProject/Sources/Controllers/Band/BandDetailsViewController.swift
+++ HomeAssginments/2.1/teamTuesday/LayoutProject/LayoutProject/Sources/Controllers/Band/BandDetailsViewController.swift
@@ -9,21 +9,62 @@ import UIKit
 
 class BandDetailsViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var topTitleLabel: UILabel!
+    
+    @IBOutlet weak var nameSubTitleLabel: UILabel!
+    
+    @IBOutlet weak var countrySubTitleLabel: UILabel!
+    
+    @IBOutlet weak var musicianSubTitleLabel: UILabel!
+    
+    @IBOutlet weak var albumsSubTitleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionSubTitleLabel: UILabel!
+    
+    @IBOutlet weak var redButton: UIButton!
+    
+    @IBOutlet weak var greenButton: UIButton!
+    
+    //MARK:Ovverridies
+    
+    override func viewDidLoad(){ super.viewDidLoad()
+        
+        topTitleLabel.text = "Hello world"
+        
+        nameSubTitleLabel.text = "Hello"
+        
+        countrySubTitleLabel.text = "UA"
+        
+        musicianSubTitleLabel.text = "5 musicians"
+        
+        albumsSubTitleLabel.text = "6 albums"
+        
+        descriptionSubTitleLabel.text = "hjdbhsdfbhesbdhsbdshdbfsdnbfsdfbshdbfhsdbghjfsbdghjfsdbgjhdbshgbdgsdbghdbghsdbh"
+        
+        imageView.image = UIImage(named: "imgBeatles")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: Action
+    
+    @IBAction func redButtonTapped(_ sender: UIButton) {
+        topTitleLabel.text = "Tapped"
+        sender.setTitle("Taped!", for: .normal)
+        
+        
     }
-    */
+    
+    @IBAction func redButtonTouchedUpOutside(_ sender: Any) {
+        topTitleLabel.text = "You tached up outside"
+    }
+    @IBAction func greenButtonTapped(_ sender: Any) {
+    }
+    @IBAction func greenButtonTouchedUpOutside(_ sender: Any) {
+    }
+    
+    // MARK: Custom private functions
+
+
 
 }
---
+++ HomeAssginments/2.1/teamTuesday/LayoutProject/LayoutProject/Sources/Model/StorageService.swift
@@ -0,0 +1,11 @@
+//
+//  StorageService.swift
+//  LayoutProject
+//
+//  Created by Maryna Snigurska on 18/12/2020.
+//
+
+import Foundation
+class StorageService {
+    
+}
