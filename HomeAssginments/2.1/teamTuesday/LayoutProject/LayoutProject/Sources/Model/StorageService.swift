c2310daf2911510a4d4d09667073977ccf716484
Mary Sneg <lazorenkoua@gmail.com>
2020-12-20 22:23:41 +0100

    add protocol and extension for StorageService

--- HomeAssginments/2.1/teamTuesday/LayoutProject/LayoutProject/Sources/Model/StorageService.swift
+++ HomeAssginments/2.1/teamTuesday/LayoutProject/LayoutProject/Sources/Model/StorageService.swift
@@ -4,7 +4,16 @@
 //
 //  Created by Maryna Snigurska on 18/12/2020.
 //
+//Protocol for Group
+protocol StorageServiceProtocol {
+    associatedtype MusiciansType: GroupProtocol
+    associatedtype AlbumsType
+    func getBand()-> Band <MusiciansType, AlbumsType>
+}
 
+//Extention for GroupProtocol
+extension StorageServiceProtocol where MusiciansType == String, AlbumsType == String{
+}
 import Foundation
 class StorageService<MusiciansType: GroupProtocol, AlbumsType>{
     private var band: [Band<MusiciansType, AlbumsType>] = []
