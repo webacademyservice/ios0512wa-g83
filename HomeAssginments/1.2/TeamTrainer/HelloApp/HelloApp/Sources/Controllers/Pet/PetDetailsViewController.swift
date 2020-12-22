6a40f48a79d0e308e0cfd44dd58397c42bc2e993
Mykhailo Vorontsov <mykhailo.vorontsov@ly.st>
2020-12-22 18:23:11 +0000

    Update app logic

--- HomeAssginments/1.2/TeamTrainer/HelloApp/HelloApp/Sources/Controllers/Pet/PetDetailsViewController.swift
+++ HomeAssginments/1.2/TeamTrainer/HelloApp/HelloApp/Sources/Controllers/Pet/PetDetailsViewController.swift
@@ -14,48 +14,30 @@ class PetDetailsViewController: UIViewController {
     @IBOutlet weak var subTitleLabel: UILabel!
     @IBOutlet weak var button: UIButton!
 
-    // Инкапсулированый сервис (задача 6)
     var petService: StorageServiceProtocol!
-
-// Не инкапсулированый массив прямо в контроллере. (без задачи 6)
-//    let pets =  [
-//        Pet(name: "Roger the Rabbit"),
-//        Pet(name: "Bucks Bunny")
-//    ]
 
     // MARK: Overrides
     override func viewDidLoad() {
-
         petService = StorageService()
+        petService.loadPets()
 
         super.viewDidLoad()
-
-        topTitleLabel.text = "Hello world!"
-
-        subTitleLabel.text = "Welcome to first IOS examnple"
-
-        imageView.image = UIImage(named: "icon")
+        refresh()
     }
 
     // MARK: Action
 
     @IBAction func buttonTapped(_ sender: UIButton) {
 
-        // С задачей 6
-        let pet = petService.getPet()
-
-        // Без задачи 6
-        // let pet = pets.randomElement()!
-
-        topTitleLabel.text = pet.name
-        subTitleLabel.text = pet.description()
-    }
-
-    @IBAction func buttonTouchedUpUotside(_ sender: Any) {
-
-        topTitleLabel.text = "Touched up outside"
+        refresh()
     }
 
     // MARK: Custom private functions
 
+    fileprivate func refresh() {
+        let pet = petService.getPet()
+        topTitleLabel.text = pet.name
+        imageView.image = pet.image ?? #imageLiteral(resourceName: "default")
+        subTitleLabel.text = pet.shortDescription
+    }
 }
--- HomeAssginments/1.2/TeamTrainer/HelloApp/HelloApp/Sources/Model/Pet.swift
+++ HomeAssginments/1.2/TeamTrainer/HelloApp/HelloApp/Sources/Model/Pet.swift
@@ -7,15 +7,42 @@
 
 import Foundation
 
-protocol PetProtocol: Equatable {
+protocol PetProtocol {
+    /**
+     Картинка. Опциональна
+     Тип Image перезадан в "UIImageExtensions.swift"
+     что бы можно было использовать в модели без подключения UIKit
+     */
+    var image: Image? { get }
+
+    /// Имя для
     var name: String { get }
-    func description() -> String
+
+    /// Короткое описание
+    var shortDescription: String { get }
+
+
+    /// теги
+    var tags: [String] { get }
 }
 
 struct Pet: PetProtocol {
-    func description() -> String {
-        return "name is \(name)"
-    }
 
     let name: String
+    let shortDescription: String
+    let image: Image?
+    let tags: [String]
+}
+
+extension Pet: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+        hasher.combine(shortDescription)
+    }
+}
+
+extension Pet: Equatable {
+    static func == (lhs: Pet, rhs: Pet) -> Bool {
+        lhs.hashValue != rhs.hashValue
+    }
 }
