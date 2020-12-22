26bdd8b5e4325e74a8036558ff57841ae604cbf8
Mary Sneg <lazorenkoua@gmail.com>
2020-12-22 20:31:38 +0100

    add remove fruit

--- HomeAssginments/3.1/TeamTuesday/FruitNinja_Snigurska/FruitNinja/FruitNinja/Sources/ViewControllers/GameViewController.swift
+++ HomeAssginments/3.1/TeamTuesday/FruitNinja_Snigurska/FruitNinja/FruitNinja/Sources/ViewControllers/GameViewController.swift
@@ -8,8 +8,11 @@
 import UIKit
 
 class GameViewController: UIViewController {
-   
+    
     var game: GameModelProtocol!
+    var fruitView: [UUID:UIView] = [:]
+    
+    
     override func viewDidLoad() {
         
         game = GameModel()
@@ -23,32 +26,48 @@ extension GameViewControllerProtocol{
     
     //MARK:GameViewControllerProtocol
     
-        func var(fruit: Fruit) {
-            
-        }
-        
-        func remove(fruit: Fruit) {
-            
-        }
-        
-        func move(fruit: Fruit) {
-            
-        }
-        
-        func update(score: Int) {
-            
-        }
-        
-        //MARK: Private
-    private viewFor(fruit:Fruit)  -> UIView {
-        return UIView ()
+    func add(fruit: Fruit) {
+        let view = viewFor(fruit: fruit)
+        view.addSubview(view)
+        self.fruitView[fruit.id] = view
         
     }
-        
-        
+    
+    func remove(fruit: Fruit) {
+        guard let viewToRemove = fruitView[fruit.id] else {return}
+        fruitView.removeValue(forKey: fruit.id)
+        viewToRemove.removeFromSuperview()
+    }
+    
+    func move(fruit: Fruit) {
         
     }
-
+    
+    func update(score: Int) {
+        
+    }
+    
+    //MARK: Private
+    private func viewFor(fruit:Fruit)  -> UIView {
+        let imageView = UIImageView(image: imageFor(kind: fruit.kind))
+        var frame = imageView.frame
+        frame.origin = translate(fruitPosition: fruit.position)
+        imageView.frame = frame
+        return imageView
+    }
+    
+    private func imageFor(kind: Fruit.Kind) -> UIImage{
+        
+    }
+    
+    private func translate(fruitPosition:CGPoint) -> CGPoint{
+        return CGPoint(
+            x: self.view.bounds.size.width * (0.5 + fruitPosition.x),
+            y: self.view.bounds.size.height * (1-fruitPosition.y)
+        )
+    }
+    
+}
 
 
 
