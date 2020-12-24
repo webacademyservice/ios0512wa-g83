e09537cd42952fdff8f51113e37414eb6c7bbef2
Mary Sneg <lazorenkoua@gmail.com>
2020-12-24 18:42:49 +0100

    fix error

--- HomeAssginments/3.1/TeamTuesday/FruitNinja_Snigurska/FruitNinja/FruitNinja/Sources/Extensions/CGPointExtension.swift
+++ HomeAssginments/3.1/TeamTuesday/FruitNinja_Snigurska/FruitNinja/FruitNinja/Sources/Extensions/CGPointExtension.swift
@@ -8,7 +8,7 @@
 import CoreGraphics
 
 extension CGPoint {
-    static func + (lhs:CGPoint, rhs:CGPoint) -> CGPoint {
-        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + lhs.y)
+    static func + (lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
     }
 }
