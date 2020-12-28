17c9ecd0e9e2c3324a152daa8a634d284c007551
Alexander Myahkov <alexandersmkv@gmail.com>
2020-12-28 21:59:33 +0200

    Team Friday Final Project ver. 0.1

---
+++ HomeAssginments/Final Projects/Team Friday/DAAD/daadTests/daadTests.swift
@@ -0,0 +1,33 @@
+//
+//  daadTests.swift
+//  daadTests
+//
+//  Created by Alex Mkoff on 12/28/20.
+//
+
+import XCTest
+@testable import daad
+
+class daadTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
