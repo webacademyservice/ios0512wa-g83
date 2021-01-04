aa6d0b6cb6f893ebc2b5d4329e2b3c9f8e7045d8
Alexander Myahkov <alexandersmkv@gmail.com>
2021-01-04 17:10:54 +0200

    New Layout

---
+++ HomeAssginments/Final Projects/Team Friday/DAAD/daad/daad/MainScreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainScreenViewController.swift
+//  daad
+//
+//  Created by Alex Mkoff on 1/3/21.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Main"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
