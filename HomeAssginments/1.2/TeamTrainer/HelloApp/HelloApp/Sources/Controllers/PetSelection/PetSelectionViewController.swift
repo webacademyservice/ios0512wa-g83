763a443dde5d41c7d1a46a3dec01c4737e0e836d
Mykhailo Vorontsov <mykhailo.vorontsov@ly.st>
2021-01-09 09:29:09 +0000

    Migrate to diffable datasource

--- HomeAssginments/1.2/TeamTrainer/HelloApp/HelloApp/Sources/Controllers/PetSelection/PetSelectionViewController.swift
+++ HomeAssginments/1.2/TeamTrainer/HelloApp/HelloApp/Sources/Controllers/PetSelection/PetSelectionViewController.swift
@@ -9,17 +9,43 @@ import UIKit
 
 class PetSelectionViewController: UIViewController {
 
+    typealias DataSource = UITableViewDiffableDataSource<Int, Pet>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<Int, Pet>
+
     @IBOutlet weak var tableView: UITableView!
+
     var petService: StorageServiceProtocol!
 
+    private lazy var dataSource: DataSource = {
+        return DataSource(tableView: tableView) { (tableView, indexPath, pet) -> UITableViewCell? in
+
+            let cell = tableView.dequeueReusableCell(withIdentifier: "PetCell", for: indexPath)
+
+            cell.textLabel?.text = pet.name
+            cell.imageView?.image = pet.image
+
+            return cell
+        }
+    }()
+
     private var pets: [Pet] {
-        self.petService.allPets
+        set {
+            var snapshot = Snapshot()
+            snapshot.appendSections([0])
+            snapshot.appendItems(newValue, toSection: 0)
+            dataSource.apply(snapshot, animatingDifferences: true, completion: nil)
+        }
+        get {
+            dataSource.snapshot().itemIdentifiers(inSection: 0)
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = dataSource
         petService = StorageService()
         petService.loadPets()
+        pets = petService.allPets
     }
 
 
@@ -40,21 +66,21 @@ class PetSelectionViewController: UIViewController {
 
 }
 
-extension PetSelectionViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-
-        return pets.count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PetCell", for: indexPath)
-
-        let pet = pets[indexPath.row]
-
-        cell.textLabel?.text = pet.name
-        cell.imageView?.image = pet.image
-
-        return cell
-    }
-}
+//extension PetSelectionViewController: UITableViewDataSource {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//
+//        return pets.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "PetCell", for: indexPath)
+//
+//        let pet = pets[indexPath.row]
+//
+//        cell.textLabel?.text = pet.name
+//        cell.imageView?.image = pet.image
+//
+//        return cell
+//    }
+//}
