eeacec7768ab02e5cb9adb633418369c14a42138
vitya <vitya1808200162@gmail.com>
2021-01-23 18:32:49 +0200

    Some code for WeatherForecast

---
+++ HomeAssginments/Final Projects/TeamMonday/WeatherForecastStoryboardProject/WeatherForecast/HourlyCollectionCell/HourlyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HourlyTableViewCell.swift
+//  WeatherForecast
+//
+//  Created by Vitya Mandryk on 21.01.2021.
+//
+
+import UIKit
+
+class HourlyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ HomeAssginments/Final Projects/TeamMonday/WeatherForecastStoryboardProject/WeatherForecast/WeatherCell/WeatherTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherTableViewCell.swift
+//  WeatherForecast
+//
+//  Created by Vitya Mandryk on 21.01.2021.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
