fa904a1bc5e5a54f97af6f10727a43b6451be000
Mykhailo Vorontsov <mykhailo.vorontsov@ly.st>
2021-01-24 09:45:46 +0000

    Add project for 7.1 sample

---
+++ HomeAssginments/7.1/FireNotes/FireNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FireNotes
+//
+//  Created by Mykhailo Vorontsov on 24/01/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
